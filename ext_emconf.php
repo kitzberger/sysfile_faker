f4a8bbf2ced44033765a20fad9c00de096a32ea4
Philipp Kitzberger <philipp@kitze.net>
2021-06-22 08:00:38 +0200

    Bumped constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ),
         'conflicts' => array(
         ),
