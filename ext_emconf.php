d333e74e408db34552ad67d939aacb28a7446367
Philipp Kitzberger <coding@kitze.net>
2024-12-27 12:34:03 +0100

    Bumped version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
         ],
